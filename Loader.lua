6cc4c587ba1d44c984fcf1c3aebb6d74a2b25206
ShimiYaBoi <121461369+ShimiYaBoi@users.noreply.github.com>
2023-03-01 20:28:08 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 662417684 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Lucky%20Block%20Battleground"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Lucky%20Block%20Battleground.lua"))()
 end
 if game.PlaceId == 10198661638 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Farm%20Factory%20Tycoon"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Farm%20Factory%20Tycoon.lua"))()
 end
