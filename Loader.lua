722c15db0024f7a2db19486fde6bde3107f704ff
ShimiYaBoi <121461369+ShimiYaBoi@users.noreply.github.com>
2023-03-07 00:51:58 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,6 +7,9 @@ end
 if game.PlaceId == 6677985923 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Millionaire%20Empire%20Tycoon.lua"))()  
 end
-if game.PlaceId == 1962086868 or game.PlaceId == 3582763398 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Tower%20Of%20Hell.lua"))()  
+if game.PlaceId == 1962086868 or game.PlaceId == 3582763398 or game.PlaceId == 5253186791 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Tower%20Of%20Hell.lua"))()
+  if game.PlaceId == 5253186791 then
+    print("Damn.... Got Banned? Well Sucks to be you")
+    end
 end
