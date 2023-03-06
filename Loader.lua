ac3909295465e796ed70c2cf2e930a1c32f347ba
ShimiYaBoi <121461369+ShimiYaBoi@users.noreply.github.com>
2023-03-07 00:44:58 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,3 +7,6 @@ end
 if game.PlaceId == 6677985923 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Millionaire%20Empire%20Tycoon.lua"))()  
 end
+if game.PlaceId == 1962086868 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Scripts/Tower%20Of%20Hell.lua"))()  
+end
