de6147f3f9957e6c52b7bd32942cb7954aba9662
ShimiYaBoi <121461369+ShimiYaBoi@users.noreply.github.com>
2023-03-28 23:09:35 +0300

    Create Pet Sim X Mini GUI.lua
    Open Source GUI Mostly I made as "Legit Mode" for Pet Sim X

---
+++ Scripts/Pet Sim X Mini GUI.lua
@@ -0,0 +1,108 @@
+--[[
+ __       ______   ______   ________   __       ______       ________   ___   __    ______       _______    __       ______    _______   ________   __       ______      
+/_/\     /_____/\ /_____/\ /_______/\ /_/\     /_____/\     /_______/\ /__/\ /__/\ /_____/\     /______/\  /_/\     /_____/\ /_______/\ /_______/\ /_/\     /_____/\     
+\:\ \    \:::_ \ \\:::__\/ \::: _  \ \\:\ \    \::::_\/_    \::: _  \ \\::\_\\  \ \\:::_ \ \    \::::__\/__\:\ \    \:::_ \ \\::: _  \ \\::: _  \ \\:\ \    \::::_\/_    
+ \:\ \    \:\ \ \ \\:\ \  __\::(_)  \ \\:\ \    \:\/___/\    \::(_)  \ \\:. `-\  \ \\:\ \ \ \    \:\ /____/\\:\ \    \:\ \ \ \\::(_)  \/_\::(_)  \ \\:\ \    \:\/___/\   
+  \:\ \____\:\ \ \ \\:\ \/_/\\:: __  \ \\:\ \____\_::._\:\    \:: __  \ \\:. _    \ \\:\ \ \ \    \:\\_  _\/ \:\ \____\:\ \ \ \\::  _  \ \\:: __  \ \\:\ \____\_::._\:\  
+   \:\/___/\\:\_\ \ \\:\_\ \ \\:.\ \  \ \\:\/___/\ /____\:\    \:.\ \  \ \\. \`-\  \ \\:\/.:| |    \:\_\ \ \  \:\/___/\\:\_\ \ \\::(_)  \ \\:.\ \  \ \\:\/___/\ /____\:\ 
+    \_____\/ \_____\/ \_____\/ \__\/\__\/ \_____\/ \_____\/     \__\/\__\/ \__\/ \__\/ \____/_/     \_____\/   \_____\/ \_____\/ \_______\/ \__\/\__\/ \_____\/ \_____\/ 
+                                                                                                                                                                         
+]]
+
+getgenv().orbs = true
+getgenv().Lootbags = true
+getgenv().CometFinder = true
+
+--[[
+ ______   __  __   ___   __    ______  _________  ________  ______   ___   __    ______      
+/_____/\ /_/\/_/\ /__/\ /__/\ /_____/\/________/\/_______/\/_____/\ /__/\ /__/\ /_____/\     
+\::::_\/_\:\ \:\ \\::\_\\  \ \\:::__\/\__.::.__\/\__.::._\/\:::_ \ \\::\_\\  \ \\::::_\/_    
+ \:\/___/\\:\ \:\ \\:. `-\  \ \\:\ \  __ \::\ \     \::\ \  \:\ \ \ \\:. `-\  \ \\:\/___/\   
+  \:::._\/ \:\ \:\ \\:. _    \ \\:\ \/_/\ \::\ \    _\::\ \__\:\ \ \ \\:. _    \ \\_::._\:\  
+   \:\ \    \:\_\:\ \\. \`-\  \ \\:\_\ \ \ \::\ \  /__\::\__/\\:\_\ \ \\. \`-\  \ \ /____\:\ 
+    \_\/     \_____\/ \__\/ \__\/ \_____\/  \__\/  \________\/ \_____\/ \__\/ \__\/ \_____\/ 
+                                                                                             
+]]
+
+function LootOrbs()
+    spawn(function()
+        while getgenv().orbs == true do wait(2)
+            for i,v in pairs(game:GetService("Workspace")["__THINGS"].Orbs:GetChildren()) do
+                if v:IsA("Part") then
+                    v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+                end
+            end
+        end
+    end)
+end
+function Loot_LootBags()
+    spawn(function()
+        while getgenv().Lootbags == true do wait(2)
+            for i,v in pairs(game:GetService("Workspace")["__THINGS"].Lootbags:GetChildren()) do
+                if v:IsA("MeshPart") then
+                    v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+                end
+            end
+        end
+    end)
+end
+function CometNotif()
+    spawn(function()
+        while getgenv().CometFinder == true do
+            task.wait(1)
+            for i,v in pairs(game:GetService("Workspace")["__THINGS"].Comets:GetChildren()) do
+                if v:IsA("MeshPart") then
+                    local notif = Instance.new("Sound")
+                    notif.Name = "Notification"
+                    notif.SoundId = "http://www.roblox.com/asset/?id=5153737200"
+                    notif.Volume = 2
+                    notif.Looped = false
+                    notif.archivable = false
+                    notif.Parent = game.Workspace
+        
+                    notif:Play()
+                    wait(4)
+                    notif:Destroy()
+                end
+            end
+        end
+    end)
+end
+
+--[[
+ __        ________   _______   ______    ________   ______    __  __    
+/_/\      /_______/\/_______/\ /_____/\  /_______/\ /_____/\  /_/\/_/\   
+\:\ \     \__.::._\/\::: _  \ \\:::_ \ \ \::: _  \ \\:::_ \ \ \ \ \ \ \  
+ \:\ \       \::\ \  \::(_)  \/_\:(_) ) )_\::(_)  \ \\:(_) ) )_\:\_\ \ \ 
+  \:\ \____  _\::\ \__\::  _  \ \\: __ `\ \\:: __  \ \\: __ `\ \\::::_\/ 
+   \:\/___/\/__\::\__/\\::(_)  \ \\ \ `\ \ \\:.\ \  \ \\ \ `\ \ \ \::\ \ 
+    \_____\/\________\/ \_______\/ \_\/ \_\/ \__\/\__\/ \_\/ \_\/  \__\/ 
+                                                                         
+]]
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Pet Sim X Auto Collector", getgenv().theme)
+
+local Tab = Window:NewTab("Main")
+
+local MainSection = Tab:NewSection("Main")
+
+MainSection:NewToggle("Orbs", "...", function(state)
+    getgenv().orbs = state
+    LootOrbs()
+end)
+
+MainSection:NewToggle("Lootbags", "...", function(state)
+    getgenv().Lootbags = state
+    Loot_LootBags()
+end)
+
+MainSection:NewToggle("Comet Notification", "...", function(state)
+    getgenv().CometFinder = state
+    CometNotif()
+end)
+
+MainSection:NewKeybind("Toggle UI", "Toggles UI", Enum.KeyCode.RightAlt, function()
+	Library:ToggleUI()
+ end)
