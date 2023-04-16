a37f1818f3e80a3e9c467990f2eefe796464e362
ShimiYaBoi <121461369+ShimiYaBoi@users.noreply.github.com>
2023-04-16 22:11:56 +0300

    Add files via upload

---
+++ !Shimi's Loader.lua
@@ -0,0 +1,7 @@
+getgenv().theme = nil
+--[[
+    There are Several Themes: "LightTheme", "DarkTheme", "GrapeTheme", "BloodTheme", "Ocean", "Midnight", "Sentinel", "Synapse", "Serpent"
+    Just Change "nil" to one of the above to change the theme of the UI
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Loader.lua"))()
