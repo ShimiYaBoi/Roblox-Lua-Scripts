35ac517d773a28be8bae980856ea39805b058660
ShimiYaBoi <121461369+ShimiYaBoi@users.noreply.github.com>
2023-04-16 22:16:12 +0300

    Update !Shimi's Loader.lua

--- !Shimi's Loader.lua
+++ !Shimi's Loader.lua
@@ -2,6 +2,16 @@ getgenv().theme = nil
 --[[
     There are Several Themes: "LightTheme", "DarkTheme", "GrapeTheme", "BloodTheme", "Ocean", "Midnight", "Sentinel", "Synapse", "Serpent"
     Just Change "nil" to one of the above to change the theme of the UI
+
+
+    Games That Work Right now:
+    1) Farm Factory Tycoon (https://www.roblox.com/games/10198661638/Farm-Factory-Tycoon)
+    2) Legends Of Speed (https://www.roblox.com/games/3101667897/Legends-Of-Speed)
+    3) Lucky Block Battleground (https://www.roblox.com/games/662417684/LUCKY-BLOCKS-Battlegrounds)
+    4) Millionaire Empire Tycoon (https://www.roblox.com/games/6677985923/X2-Millionaire-Empire-Tycoon)
+    5) Pet Sim X (Very Minimal and 2 features | Might Update in the future) (https://www.roblox.com/games/6284583030/MINE-Pet-Simulator-X)
+    6) Tower Of Hell (https://www.roblox.com/games/1962086868/Tower-of-Hell)
+    7) Rarity Factory Tycoon (https://www.roblox.com/games/10919241870/EASTER-Rarity-Factory-Tycoon)
 ]]
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ShimiYaBoi/Roblox-Lua-Scripts/main/Loader.lua"))()
